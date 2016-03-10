da1c56a35a73a4f5a57d0bb7147169b0d2b31c9e
spbrown5 <spbrown5@bellsouth.net>
2016-03-10 15:07:07 -0500

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -33,7 +33,7 @@ var MapsLib = {
 
   //*MODIFY Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/
   //*Important* this key is for demonstration purposes. please register your own.
-  googleApiKey:       "AIzaSyAwwRhdqX4ZATPhGKWofTnBn3U8aLbbe9U",
+  googleApiKey:       "AIzaSyCy4cNtfENpveN3UQmdwumU6c303DalO4c",
 
   //MODIFY name of the location column in your Fusion Table.
   //NOTE: if your location column name has spaces in it, surround it with single quotes
